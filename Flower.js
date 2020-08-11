4571bc5f352cb358490e7641bb4c846c6a805278
shaoli-lu <shaoli_lu@yahoo.com>
2020-08-10 19:48:01 -0500

    Add more animation

--- Flower.js
+++ Flower.js
@@ -1,11 +1,23 @@
+
 var element = document.createElement("img");
+
 element.src = "Flower.jpg";
 document.body.appendChild(element);
-element.style.opacity = 0;
+
 element.style.transition = "opacity 10s";
 
+element.style.opacity =0;
+	
 function act() {
-  let bw = document.getElementById('bw')
-  bw.innerHTML = "";
-  element.style.opacity = 1;
+	let en = document.getElementById('ename')
+	setInterval(function(){en.style.color = "blue";}, 3000)
+	let cn = document.getElementById('cname')
+	setInterval(function(){cn.style.color = "red";}, 5000)
+	setInterval(function(){cn.style.fontweight = "500";}, 7000)
+	let tn = document.getElementById('tname')
+	let sn = document.getElementById('sname')
+	setInterval(function(){sn.style.color = "blue";}, 9000)
+	setInterval(function(){tn.style.color = "red";}, 12000)
+	
+	
 }
